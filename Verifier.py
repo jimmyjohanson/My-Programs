838c5b6f313caa0eae651b63260ddc8bad2a75a3
jimmyjohanson <87723708+jimmyjohanson@users.noreply.github.com>
2021-07-21 03:24:04 +0530

    Create Verifier.py

---
+++ Verifier.py
@@ -0,0 +1,8 @@
+x=input("Input Your encoded flag : ")
+print("")
+answer = "ZmxhZ3tHMXRIdWJfYzBtbWl0XyFzX1B1cjNfbDB2M30="
+
+if x == answer:
+  print("Here is your Secret")
+else:
+  print("Wrong Answer!!")
