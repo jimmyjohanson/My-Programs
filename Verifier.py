57bce4986b3596d0128b97f08c797a0357e35c6d
jimmyjohanson <87723708+jimmyjohanson@users.noreply.github.com>
2021-07-21 03:25:03 +0530

    Update Verifier.py

--- Verifier.py
+++ Verifier.py
@@ -1,6 +1,6 @@
 x=input("Input Your encoded flag : ")
 print("")
-answer = "ZmxhZ3tHMXRIdWJfYzBtbWl0XyFzX1B1cjNfbDB2M30="
+answer = "ZmxhZ3twc3VlZG9fZmxhZ30="
 
 if x == answer:
   print("Here is your Secret")
